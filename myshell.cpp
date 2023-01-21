b90c499a07aaf9262e037ebda0b05c114ac98e77
yalemu <93763372+yalemu@users.noreply.github.com>
2023-01-21 15:37:20 -0800

    Update myshell.cpp

--- myshell.cpp
+++ myshell.cpp
@@ -6,16 +6,4 @@
 using namespace std;
 
 int main(){
-    string line = "Hello world!";
-    vector <string> tokens;
-    stringstream check1(line);
-    string something;
-
-    while (getline(check1,something,' '))
-    {
-        tokens.push_back(something);
-    }
-    for(int i = 0; i < tokens.size();i++)
-    cout << tokens[i] << endl;
-    return 0;
-}
+    cout << "myshell$";
