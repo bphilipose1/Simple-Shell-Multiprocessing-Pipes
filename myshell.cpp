4cdc9448a7dba2c72338a10ee7a2924432ed2e2a
Benjamin Philipose <philiposebenjamin@gmail.com>
2023-01-21 18:47:50 -0800

    Completed Parse Code without test cases

--- myshell.cpp
+++ myshell.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 int main(int argc, char* argv[])    {
     //creating buffer for tokens
-   
     const int max_NUM = 20;
     const int max_SZ = 20;
     string buf[max_SZ][max_NUM];
+
+    //counters to detail buf utilized size  **includes \0 at end of row**
     int rowCnt = 0;
     int clmnCnt = 0;
    
@@ -16,51 +17,27 @@ int main(int argc, char* argv[])    {
     cout << "myshell$";
     getline(cin, inputCMD);
 
-    //test input COPY AND PASTE WHEN PROMPTED :  ls -alF / | grep bin | tr a-z A-Z | rev | cat -n
-    inputCMD += " ";    // space indicates end of string
+    inputCMD += " ";  // added space indicating end of string
     string token;
 
 
     for (auto tempTok : inputCMD)    {  // itereates through each character from input string
-        if (tempTok == ' ' && token.length() == 0)  {//do nothing if it reads space and nothing is in token (also deals with extra spaces between command tokens)
-            cout << "With \"" << tempTok << "\" it ended in CASE 1" << endl;
+        if (tempTok == ' ' && token.length() == 0)  {//do nothing if it reads space and nothing is in token (also deals with extra spaces between command tokens)          
             continue;  
-            
         }
         else if (tempTok == ' ' && token.length() != 0) {   //space after a word indicates it has been put fully in token therefore add to 2D array
-            cout << "With \"" << tempTok << "\" it ended in CASE 2" << endl;
             buf[rowCnt][clmnCnt] = token;
             clmnCnt++;
-            token = ""; //clears up token for next word
-            
+            token = ""; //clears up token for next word        
         }
         else if(tempTok == '|') {   //if pipe is read it adds "\0" to signify end of the row and changes write to new row to signify new process command
-            cout << "With \"" << tempTok << "\" it ended in CASE 3" << endl;
             clmnCnt++;
             buf[rowCnt][clmnCnt] = "\0";
             rowCnt++;
-            clmnCnt = 0;
-            
+            clmnCnt = 0;        
         }
-        else    {   //iterates through characters of a word
-            cout << "With \"" << tempTok << "\" it ended in CASE 4" << endl;
-            token += tempTok;//combines characters to form a token 
-            
+        else    {   //adds parsed characters to rebuild word as a token from input string
+            token += tempTok;//combines characters to form a token        
         }
     }
-
-    //testing 2D array contents
-    cout << "\ntime to test array :(" << rowCnt << ")("<< clmnCnt <<")" << endl;
-    for (int i = 0; i <= rowCnt; i++)
-    {
-        cout << "|";
-        for (int j = 0; j <= clmnCnt; j++)
-        {
-            if(buf[i][j] == "\0")
-                continue;
-            cout << buf[i][j] << "|";
-        }
-        cout << "" << endl;
-    }
-    
 }
